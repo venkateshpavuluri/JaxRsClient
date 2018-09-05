9efc3e847abf9217cd816da50e58446967f0b46c
Venkatesh <pavuluri.venki@gmail.com>
2018-09-05 23:41:42 +0530

    updated for releases

--- src/com/async/response/client/AsynchronousResponseClient.java
+++ src/com/async/response/client/AsynchronousResponseClient.java
@@ -28,7 +28,7 @@ public static void main(String[] args) {
 	
 	
 	System.out.println("Independent of Asyncronoustyu call1");
-	System.out.println("Independent of Asyncronous call2");
+	System.out.println("Independent of Asyncronous releases call2");
 	System.out.println("Independent of Asyncronous call3");
 	System.out.println("Independent of Asyncronous call4");
 	System.out.println("Independent of Asyncronous call5");
